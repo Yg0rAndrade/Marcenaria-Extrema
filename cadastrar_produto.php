9fecd59807f8f5f9f6b66d76fbd6c0076f7186b0
Mik785 <mickael.m.mineo01@gmail.com>
2024-10-12 10:23:40 -0300

    Foi criado uma página para testar o commit do github. Esta pagina será desenvolvida para cadastrar produtos.

---
+++ cadastrar_produto.php
@@ -0,0 +1,19 @@
+<?php
+    #require_once 'databse.php';
+    #require_once 'produto.php';
+    
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Produtos</title>
+</head>
+<body>
+    <h1>WIP</h1>
+    <h6>Esta página irá cadastrar um produto</h6>
+    
+</body>
+</html>
