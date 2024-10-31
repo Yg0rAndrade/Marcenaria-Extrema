44b66b1b01ef8fa3bc275eacbd707b2732f15df5
Hiago  Henrique Tavares <147439502+HiagoTavares@users.noreply.github.com>
2024-10-31 20:52:34 -0300

    nada de mais
    a

---
+++ app/Model/db.php
@@ -0,0 +1,7 @@
+<?php
+
+    $apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZnRxZmNpemd2eG50Y2lweHZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2MDYwNDcsImV4cCI6MjA0NDE4MjA0N30.NHgOyD8rgby8R5n0ebhAe_sdle5wpUzNgwI8oF4ezt4';
+
+    $authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZnRxZmNpemd2eG50Y2lweHZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2MDYwNDcsImV4cCI6MjA0NDE4MjA0N30.NHgOyD8rgby8R5n0ebhAe_sdle5wpUzNgwI8oF4ezt4';
+
+    $supabaseURL = 'https://inftqfcizgvxntcipxvr.supabase.co/rest/v1';
